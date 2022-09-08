34deedd47c19d9481b514cdd0fe94dc1dff866db
vasant-deb <iamvasantsharma@gmail.com>
2022-09-08 01:43:30 -0600

    new

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+
 
     
 @Component({
@@ -10,17 +10,12 @@ import { ApiService } from '../api.service';
 
 export class LoginComponent implements OnInit {
 
-  story:any;
  
-  constructor(private api:ApiService) {}
+ 
+  constructor() {}
   ngOnInit() {
-    
-      this.api.getstory()
-      .subscribe(response=> {
-        
-      this.story=response;
-      this.story=this.story.stats;
-  });
+     
+  
   }
 
 
