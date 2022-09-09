344422a4acb8a330161972782d70d1d1e0b89d13
vasant-deb <iamvasantsharma@gmail.com>
2022-09-09 00:41:33 -0600

    buildissue

---
+++ src/app/plugins/index.ts
@@ -0,0 +1,15 @@
+import { Plugin } from "prosemirror-state";
+
+import { image } from "ngx-editor/plugins";
+
+const getPlugins = (): Plugin[] => {
+  const plugins = [
+    image({
+      resize: true
+    })
+  ];
+
+  return plugins;
+};
+
+export default getPlugins();
